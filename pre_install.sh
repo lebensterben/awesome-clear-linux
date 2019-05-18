34b1c225b53aa80f44f361211ff8701c21fc041b
lebensterben <lebensterben@users.noreply.github.com>
2019-05-18 02:56:58 -0400

    - Added: Disable nouveau driver - Modified: Remind the user to reboot instead of ask user whether to reboot now

--- pre_install.sh
+++ pre_install.sh
@@ -16,13 +16,16 @@ unset VARIANT
 echo -e "\e[33m\xe2\x8f\xb3 Updating Clear Linux OS bootloader ...\e[m"
 clr-boot-manager update
 
-## Ask the user whether he wants to reboot now
-read -p "$(echo -e '\e[31m\xe2\x9d\x93 Do you want to reboot now (y/N)? \e[m')" -n 1 -r
-echo
-if [[ $REPLY =~ ^[y]$ ]]; then
-  echo -e "\e[33m\xe2\x8f\xb3 Rebooting ...\e[m"
-  reboot
-else
-  echo -e "\e[32mPlease reboot your system ASAP and execute the \e[33minstall.sh \e[32mscript to install the NVIDIA proprietary driver.\e[m"
-  exit 0
+## Disable nouveau driver
+echo -e "\e[33m\xe2\x8f\xb3 Disabling nouveau Driver ...\e[m"
+if [ ! -d /etc/modprobe.d ]; then
+  mkdir /etc/modprobe.d
 fi
+cat <<EOF > /etc/modprobe.d/disable-nouveau.conf
+blacklist nouveau
+options nouveau modeset=0
+EOF
+
+## Ask the user whether he wants to reboot now
+echo -e "\e[32mPlease reboot your system ASAP and execute the \e[33minstall.sh \e[32mscript to install the NVIDIA proprietary driver.\e[m"
+exit 0
