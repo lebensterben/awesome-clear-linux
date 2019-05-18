ea9af561a4278bdfa209c206d7e72538e7f5dfc6
lebensterben <lebensterben@users.noreply.github.com>
2019-05-18 03:07:23 -0400

    - Fixed: Wrong command for nvidia-uninstall - Modified: Remind the user to reboot instead of ask the use for a choice

--- uninstall.sh
+++ uninstall.sh
@@ -18,15 +18,8 @@ unset i
 
 ## Running nvidia-uninstall script (by NVIDIA) to uninstall the GPU driver
 echo -e "\e[33m\xe2\x8f\xb3 Running nvidia-uninstall ...\e[m"
-nvidia-uninstall
+/opt/nvidia/bin/nvidia-uninstall
 
 ## Ask the user whether he wants to reboot now
-read -p "$(echo -e '\e[31m\xe2\x9d\x93 Do you want to reboot now (y/N)? \e[m')" -n 1 -r
-echo
-if [[ $REPLY =~ ^[y]$ ]]; then
-  echo -e "\e[33m\xe2\x8f\xb3 Rebooting ...\e[m"
-  reboot
-else
-  echo -e "\e[32mPlease reboot your system ASAP.\e[m"
-  exit 0
-fi
+echo -e "\e[32mPlease reboot your system ASAP.\e[m"
+exit 0
