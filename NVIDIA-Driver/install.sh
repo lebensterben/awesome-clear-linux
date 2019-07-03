9fdd65d840b9832725bb4833e1a64df277106fdb
Lucius Hu <lebensterben@users.noreply.github.com>
2019-07-02 21:23:30 -0400

    Changed order of install.sh
    Now it looks for installer before updating `ldconfig`

--- NVIDIA-Driver/install.sh
+++ NVIDIA-Driver/install.sh
@@ -6,22 +6,6 @@ if [ "$(whoami)" != 'root' ]; then
   exit 1
 fi
 
-## Configure the dynamic linker configuration to include /opt/nvidia/lib and /opt/nvidia/lib32
-echo -e "\e[33m\xe2\x8f\xb3 Configuring dynamic linker configuration ...\e[m"
-if [ ! -d /etc/ld.so.conf.d ]; then
-  mkdir /etc/ld.so.conf.d
-fi
-cat <<EOF > /etc/ld.so.conf.d/nvidia.conf
-/opt/nvidia/lib
-/opt/nvidia/lib32
-EOF
-
-if [ ! -f /etc/ld.so.conf ] || [ "$(grep 'include /etc/ld\.so\.conf\.d/\*\.conf' /etc/ld.so.conf )" = '' ]; then
-  cat <<EOF >> /etc/ld.so.conf
-include /etc/ld.so.conf.d/*.conf
-EOF
-fi
-
 ## Try to locate NVIDIA Linux Driver installer
 ## `NVIDIA-Linux-x86_64-<VERESION>.run` under current directory
 ## or `~/Downloads` directory
@@ -44,6 +28,23 @@ if [ "$INSTALLER" = '' ]; then
       fi
     fi
   fi
+fi
+
+
+## Configure the dynamic linker configuration to include /opt/nvidia/lib and /opt/nvidia/lib32
+echo -e "\e[33m\xe2\x8f\xb3 Configuring dynamic linker configuration ...\e[m"
+if [ ! -d /etc/ld.so.conf.d ]; then
+  mkdir /etc/ld.so.conf.d
+fi
+cat <<EOF > /etc/ld.so.conf.d/nvidia.conf
+/opt/nvidia/lib
+/opt/nvidia/lib32
+EOF
+
+if [ ! -f /etc/ld.so.conf ] || [ "$(grep 'include /etc/ld\.so\.conf\.d/\*\.conf' /etc/ld.so.conf )" = '' ]; then
+  cat <<EOF >> /etc/ld.so.conf
+include /etc/ld.so.conf.d/*.conf
+EOF
 fi
 
 ## Install the NVIDIA driver with advanced options below
