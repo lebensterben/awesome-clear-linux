77f56c5400e063e12de913f8c6f2fa90ea66faab
Chadwick Ferguson <chadwick.ferguson@gmail.com>
2020-03-06 13:31:14 -0800

    Update install.bash

--- NVIDIA-Driver/install.bash
+++ NVIDIA-Driver/install.bash
@@ -68,6 +68,9 @@ echo -e "\e[32m Please run the \e[33mpost_install.sh \e[32mto validate that the 
 echo -e "\e[32m The version of the driver is \e[33m""$([[ "$INSTALLER" =~ ^.*\-(.*)\.run$ ]] && echo "${BASH_REMATCH[1]}")\e[m"
 read -rp "Press any key to continue ... " -n1 -s
 echo
+if [ ! -d /etc/nvidia/nvidia-application-profiles-rc.d ]; then
+  mkdir -p /etc/nvidia/nvidia-application-profiles-rc.d
+fi
 if ! sh "$INSTALLER" \
     --utility-prefix=/opt/nvidia \
     --opengl-prefix=/opt/nvidia \
@@ -77,8 +80,8 @@ if ! sh "$INSTALLER" \
     --x-module-path=/opt/nvidia/lib64/xorg/modules \
     --x-library-path=/opt/nvidia/lib64 \
     --x-sysconfig-path=/etc/X11/xorg.conf.d \
-    --documentation-prefix=/opt/nvidia \
-    --application-profile-path=/etc/nvidia \
+    --documentation-prefix=/opt/nvidia \     
+    --application-profile-path=/etc/nvidia/nvidia-application-profiles-rc.d \
     --no-precompiled-interface \
     --no-distro-scripts \
     --force-libglx-indirect \
