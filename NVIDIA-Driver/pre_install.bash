6395f768bd3e0c6db5000cff8d2389b8462a8bfa
Lucius Hu <lebensterben@users.noreply.github.com>
2020-03-10 03:07:47 -0400

    Changed NVIDIA-Driver/pre_install.bash
    - It no longer checks `sudo` privilege in the beginning

--- NVIDIA-Driver/pre_install.bash
+++ NVIDIA-Driver/pre_install.bash
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
-# Make sure to have root privilege
-if [ "$(whoami)" != 'root' ]; then
-  echo -e "\e[31m\xe2\x9d\x8c Please retry with root privilege.\e[m"
-  exit 1
-fi
-
 # Make sure `IOMMU` is disabled for Intel CPUs
 if grep -q 'Intel' /proc/cpuinfo; then
   echo -e "\e[33m\xe2\x8f\xb3 Found \e[32mIntel CPU(s)\e[33m, disabling IOMMU ...\e[m"
   if [ ! -d /etc/kernel/cmdline-removal.d ]; then
-    mkdir -p /etc/kernel/cmdline-removal.d
+    sudo mkdir -p /etc/kernel/cmdline-removal.d
   fi
-  cat <<< 'intel_iommu=igfx_off' > /etc/kernel/cmdline-removal.d/intel-iommu.conf
+  cat <<< 'intel_iommu=igfx_off' | \
+    sudo tee /etc/kernel/cmdline-removal.d/intel-iommu.conf > /dev/null
 fi
 
 # Create a systemd unit that overwrites `libGL` library after every OS update
@@ -23,7 +18,7 @@ echo -e "\e[33m\xe2\x8f\xb3 Creating a systemd unit that fix problems with \"lib
 echo -e "\e[33m Writing the systemd unit file at \"\e[32m\
 /etc/systemd/system/fix-nvidia-libGL-trigger.service\e[33m\"\e[m"
 
-cat <<EOF > /etc/systemd/system/fix-nvidia-libGL-trigger.service
+cat <<EOF | sudo tee /etc/systemd/system/fix-nvidia-libGL-trigger.service > /dev/null
 [Unit]
 Description=Fixes libGL symlinks for the NVIDIA proprietary driver
 BindsTo=update-triggers.target
@@ -36,19 +31,19 @@ EOF
 
 ## Reload systemd daemon to find the new service
 echo -e "\e[33m Reload systemd manager configuration to pick up the new service ...\e[m"
-systemctl daemon-reload
+sudo systemctl daemon-reload
 
 ## Make sure the service is launched after every OS update
 echo -e "\e[33m Creating a hook to Clear Linux OS updates ...\e[m"
-systemctl add-wants update-triggers.target fix-nvidia-libGL-trigger.service
+sudo systemctl add-wants update-triggers.target fix-nvidia-libGL-trigger.service
 
 # Install Dynamic Kernel Module System (DKMS) if not found, according to kernel variant
 VARIANT="$(uname -r)" && VARIANT=${VARIANT##*.}
 case "$VARIANT" in
   native|lts)
-    if ! swupd bundle-list | grep -q kernel-"$VARIANT"-dkms; then
+    if ! sudo swupd bundle-list | grep -q kernel-"$VARIANT"-dkms; then
       echo -e "\e[33m\xe2\x8f\xb3 Installing Dynamic Kernel Module System ...\e[m"
-      swupd bundle-add kernel-"$VARIANT"-dkms
+      sudo swupd bundle-add kernel-"$VARIANT"-dkms
     fi
     ;;
   *)
@@ -60,14 +55,14 @@ esac
 
 ## Update Clear Linux OS bootloader
 echo -e "\e[33m\xe2\x8f\xb3 Updating Clear Linux OS bootloader ...\e[m"
-clr-boot-manager update
+sudo clr-boot-manager update
 
 # Disable nouveau driver
 echo -e "\e[33m\xe2\x8f\xb3 Disabling nouveau Driver ...\e[m"
 if [ ! -d /etc/modprobe.d ]; then
-  mkdir /etc/modprobe.d
+  sudo mkdir /etc/modprobe.d
 fi
-cat <<EOF > /etc/modprobe.d/disable-nouveau.conf
+cat <<EOF | sudo tee /etc/modprobe.d/disable-nouveau.conf > /dev/null
 blacklist nouveau
 options nouveau modeset=0
 EOF
