8d0ef7179462d8ab7178f3364fffa9fe13268c95
Lucius Hu <lebensterben@users.noreply.github.com>
2020-03-10 02:18:58 -0400

    Updated NVIDIA-Driver/post_install.bash
    - Removed the check for `sudo` privilege
    - Improved the check for nvidia kernel modules, the script would exit with a
      warning if nothing shows up in `lsmod`
    - Removed the section for `swupd repair`, in accordance to official tutorial
    - Removed the section for `flatpak update`, since the official tutorial does
      not has this
    - Added a section to prompt the user for adding a desktop file for
      `nvidia-settings`

--- NVIDIA-Driver/post_install.bash
+++ NVIDIA-Driver/post_install.bash
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
 
-## Make sure to have root privilege
-if [ "$(whoami)" != 'root' ]; then
-  echo -e "\e[31m\xe2\x9d\x8c Please retry with root privilege.\e[m"
+# Validate that nvidia kernel modules are loaded
+echo -e "\e[33m\xe2\x8f\xb3 Making sure NVIDIA kernel modules are loaded ...\e[m"
+if ! lsmod | grep ^nvidia; then
+  echo -e "\e[31m Cannot find NVIDIA modules, something went wrong!\e[m"
   exit 1
 fi
 
-## Validate that nvidia kernel modules are loaded
-echo -e "\e[33m\xe2\x8f\xb3 Making sure NVIDIA kernel modules are loaded ...\e[m"
-lsmod | grep ^nvidia
-
-## Verify and fix OpenGL library files files that are likely modified by NVIDIA installer
-echo -e "\e[33m\xe2\x8f\xb3 Verifying the integrity of OpenGL library files ...\e[m"
-swupd verify --fix --quick --bundles=lib-opengl
-
-## Update flatpak runtime for the new NVIDIA driver
-echo -e "\e[33m\xe2\x8f\xb3 Updating the flatpak runtime ...\e[m"
-flatpak update -y
+# Optionally ask user whether to add a desktop file for "nvidia-settings"
+read -rp "Do you want to add a desktop file for \"nvidia-settings\"? (Y/n)" -n1 -s
+echo
+if [ "$REPLY" = Y ]; then
+  ln -sv /opt/nvidia/share/applications/nvidia-settings.desktop "$HOME"/.local/share/applications
+fi
 
 echo -e "\e[32m\xf0\x9f\x91\x8f Installation completed!\e[m"
