72c67a3651069be21908564e8387a459faf9a356
Lucius Hu <lebensterben@users.noreply.github.com>
2020-03-10 18:33:24 -0400

    Updated NVIDIA-Driver/post_update.bash
    - Removed the section that runs `swupd verify` to fix OpenGL library files since
      it's no longer needed
    - Removed the check for `root` privilege in the beginning
    - Re-formatted the comments

--- NVIDIA-Driver/post_update.bash
+++ NVIDIA-Driver/post_update.bash
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 
-## Make sure to have root privilege
-if [ "$(whoami)" != 'root' ]; then
-  echo -e "\e[31m\xe2\x9d\x8c Please retry with root privilege.\e[m"
+# Validate that nvidia kernel modules are loaded
+echo -e "\e[33m\xe2\x8f\xb3 Making sure NVIDIA kernel modules are loaded ...\e[m"
+if ! lsmod | grep ^nvidia; then
+  echo -e "\e[31m Cannot find NVIDIA modules, something went wrong!\e[m"
   exit 1
 fi
 
-## Validate that nvidia kernel modules are loaded
-echo -e "\e[33m\xe2\x8f\xb3 Making sure NVIDIA kernel modules are loaded ...\e[m"
-lsmod | grep ^nvidia
-
-## Verify and fix OpenGL library files files that are likely modified by NVIDIA installer
-echo -e "\e[33m\xe2\x8f\xb3 Verifying the integrity of OpenGL library files ...\e[m"
-swupd verify --fix --quick --bundles=lib-opengl
-
-## Update flatpak runtime for the new NVIDIA driver
+# Update flatpak runtime for the new NVIDIA driver
 echo -e "\e[33m\xe2\x8f\xb3 Updating the flatpak runtime ...\e[m"
 flatpak update -y
 
