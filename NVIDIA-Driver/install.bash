f113fa01d47e1bff09279d66e62e247a1aee1632
Lucius Hu <lebensterben@users.noreply.github.com>
2020-03-09 20:40:54 -0400

    Updated NVIDIA-Driver/pre_install.bash
    - Added two new sections that:
      - Disable `Intel IOMMU` for Intel CPUs
      - Add systemd service that fix the problem with `libGL` library
    - Re-formatted the comments
    - Fixed a typo

--- NVIDIA-Driver/install.bash
+++ NVIDIA-Driver/install.bash
@@ -1,37 +1,33 @@
 #!/usr/bin/env bash
 
-## Make sure to have root privilege
+# Make sure to have root privilege
 if [ "$(whoami)" != 'root' ]; then
   echo -e "\e[31m\xe2\x9d\x8c Please retry with root privilege.\e[m"
   exit 1
 fi
 
-## Try to locate NVIDIA Linux Driver installer
-## `NVIDIA-Linux-x86_64-<VERESION>.run` under current directory
-## or `~/Downloads` directory
+# Locate the driver installer under current directory, or download one if not found
 echo -e "\e[33m\xe2\x8f\xb3 Locating NVIDIA-Linux-x86_64-<VERSION>.run ...\e[m"
 INSTALLER="$(find "$PWD" -maxdepth 1 -name 'NVIDIA-Linux-x86_64*\.run' | sort -r | head -1 )"
 if [ "$INSTALLER" = '' ]; then
-  INSTALLER="$(find "$HOME"/Downloads -maxdepth 1 -name 'NVIDIA-Linux-x86_64*\.run' | sort -r | head -1 )"
-  if [ "$INSTALLER" = '' ]; then
-    echo -e "\e[31m\xe2\x9d\x8c Cannot find NVIDIA-Linux-x86_64-<VERSION>.run under current directory or ~/Downloads\e[m"
-    LATEST="$(curl -L https://download.nvidia.com/XFree86/Linux-x86_64 | grep "<span class='dir'>" | tail -n1 | sed -e "s/.*'>//" -e "s/\/<.*//" )"
-    if [ -z "$LATEST" ]; then
-      echo -e "\e[31m Cannot obtaining latest NVIDIA driver version number ...\e[m"
-      echo -e "\e[32m Please Download the latest driver manually\e[m"
-      exit 1
-    else
-      echo -e "\e[32m The latest version of NVIDIA driver is \e[33m${LATEST}\e[m"
-      echo -e "\e[32m Dowloading \e[33m${LATEST} ...\e[m"
-      curl -O "https://download.nvidia.com/XFree86/Linux-x86_64/${LATEST}/NVIDIA-Linux-x86_64-${LATEST}.run"
-      if [ -f "NVIDIA-Linux-x86_64-${LATEST}.run" ]; then
-        INSTALLER="NVIDIA-Linux-x86_64-${LATEST}.run"
-      fi
+  ## Cannot find installer, download one
+  echo -e "\e[31m\xe2\x9d\x8c Cannot find NVIDIA-Linux-x86_64-<VERSION>.run under current directory or ~/Downloads\e[m"
+  LATEST="$(curl -L https://download.nvidia.com/XFree86/Linux-x86_64 | grep "<span class='dir'>" | tail -n1 | sed -e "s/.*'>//" -e "s/\/<.*//" )"
+  if [ -z "$LATEST" ]; then
+    echo -e "\e[31m Cannot obtaining latest NVIDIA driver version number ...\e[m"
+    echo -e "\e[32m Please Download the latest driver manually\e[m"
+    exit 1
+  else
+    echo -e "\e[32m The latest version of NVIDIA driver is \e[33m${LATEST}\e[m"
+    echo -e "\e[32m Dowloading \e[33m${LATEST} ...\e[m"
+    curl -O "https://download.nvidia.com/XFree86/Linux-x86_64/${LATEST}/NVIDIA-Linux-x86_64-${LATEST}.run"
+    if [ -f "NVIDIA-Linux-x86_64-${LATEST}.run" ]; then
+      INSTALLER="NVIDIA-Linux-x86_64-${LATEST}.run"
     fi
   fi
 fi
 
-## Configure the dynamic linker configuration to include /opt/nvidia/lib and /opt/nvidia/lib32
+# Configure the dynamic linker configuration to include /opt/nvidia/lib and /opt/nvidia/lib32
 echo -e "\e[33m\xe2\x8f\xb3 Configuring dynamic linker configuration ...\e[m"
 if [ ! -f /etc/ld.so.conf ] || [ "$(grep 'include /etc/ld\.so\.conf\.d/\*\.conf' /etc/ld.so.conf )" = '' ]; then
   cat <<EOF >> /etc/ld.so.conf
--- NVIDIA-Driver/pre_install.bash
+++ NVIDIA-Driver/pre_install.bash
@@ -1,20 +1,59 @@
 #!/usr/bin/env bash
 
-## Make sure to have root privilege
+# Make sure to have root privilege
 if [ "$(whoami)" != 'root' ]; then
   echo -e "\e[31m\xe2\x9d\x8c Please retry with root privilege.\e[m"
   exit 1
 fi
 
-## Install Dynamic Kernel Module System (DKMS) according to kernel variant
+# Make sure `IOMMU` is disabled for Intel CPUs
+if grep -q 'Intel' /proc/cpuinfo; then
+  echo -e "\e[33m\xe2\x8f\xb3 Found \e[32mIntel CPU(s)\e[33m, disabling IOMMU ...\e[m"
+  if [ ! -d /etc/kernel/cmdline-removal.d ]; then
+    mkdir -p /etc/kernel/cmdline-removal.d
+  fi
+  cat <<< 'intel_iommu=igfx_off' > /etc/kernel/cmdline-removal.d/intel-iommu.conf
+fi
+
+# Create a systemd unit that overwrites `libGL` library after every OS update
+echo -e "\e[33m\xe2\x8f\xb3 Creating a systemd unit that fix problems with \"libGL\" library ...\
+\e[m"
+
+## Write the systemd file at "/etc/systemd/system/fix-nvidia-libGL-trigger.service"
+echo -e "\e[33m Writing the systemd unit file at \"\e[32m\
+/etc/systemd/system/fix-nvidia-libGL-trigger.service\e[33m\"\e[m"
+
+cat <<EOF > /etc/systemd/system/fix-nvidia-libGL-trigger.service
+[Unit]
+Description=Fixes libGL symlinks for the NVIDIA proprietary driver
+BindsTo=update-triggers.target
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/ln -sfv /opt/nvidia/lib/libGL.so.1 /usr/lib/libGL.so.1
+ExecStart=/usr/bin/ln -sfv /opt/nvidia/lib32/libGL.so.1 /usr/lib32/libGL.so.1
+EOF
+
+## Reload systemd daemon to find the new service
+echo -e "\e[33m Reload systemd manager configuration to pick up the new service ...\e[m"
+systemctl daemon-reload
+
+## Make sure the service is launched after every OS update
+echo -e "\e[33m Creating a hook to Clear Linux OS updates ...\e[m"
+systemctl add-wants update-triggers.target fix-nvidia-libGL-trigger.service
+
+# Install Dynamic Kernel Module System (DKMS) if not found, according to kernel variant
 VARIANT="$(uname -r)" && VARIANT=${VARIANT##*.}
-case "$VARIANT" in 
+case "$VARIANT" in
   native|lts)
-    echo -e "\e[33m\xe2\x8f\xb3 Installing Dynamic Kernel Module System ...\e[m"
-    swupd bundle-add kernel-"$VARIANT"-dkms
+    if ! swupd bundle-list | grep -q kernel-"$VARIANT"-dkms; then
+      echo -e "\e[33m\xe2\x8f\xb3 Installing Dynamic Kernel Module System ...\e[m"
+      swupd bundle-add kernel-"$VARIANT"-dkms
+    fi
     ;;
   *)
-    echo -e "\e[31m\xe2\x9d\x8c The kernel must be either \"native\" or \"lts\".\e[m"
+    echo -e "\e[31m\xe2\x9d\x8c The kernel must be either \"\e[32mnative\e[31m\" or \"\e[32mlts\
+\e[31m\".\e[m"
     exit 1
     ;;
 esac
@@ -23,7 +62,7 @@ esac
 echo -e "\e[33m\xe2\x8f\xb3 Updating Clear Linux OS bootloader ...\e[m"
 clr-boot-manager update
 
-## Disable nouveau driver
+# Disable nouveau driver
 echo -e "\e[33m\xe2\x8f\xb3 Disabling nouveau Driver ...\e[m"
 if [ ! -d /etc/modprobe.d ]; then
   mkdir /etc/modprobe.d
@@ -33,6 +72,7 @@ blacklist nouveau
 options nouveau modeset=0
 EOF
 
-## Ask the user whether he wants to reboot now
-echo -e "\e[32m Please reboot your system ASAP and execute the \e[33minstall.sh \e[32mscript to install the NVIDIA proprietary driver.\e[m"
+# Ask the user whether he wants to reboot now
+echo -e "\e[32m Please reboot your system ASAP and execute the \e[33minstall.bash \e[32mscript to \
+install the NVIDIA proprietary driver.\e[m"
 exit 0
