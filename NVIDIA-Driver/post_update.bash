bde3b87ece575b56c771a8e57a29bc1c12a43144
Lucius Hu <lebensterben@users.noreply.github.com>
2020-04-14 15:15:58 -0400

    Improved `post_update.bash`
    Same as `post_install.bash`
    - Added a section to ask user whether to add a desktop file for
      `nvidia-settings`
    - Added a section to ask user whether to remove NVIDIA installer file(s)

--- NVIDIA-Driver/post_update.bash
+++ NVIDIA-Driver/post_update.bash
@@ -11,4 +11,21 @@ fi
 echo -e "\e[33m\xe2\x8f\xb3 Updating the flatpak runtime ...\e[m"
 flatpak update -y
 
+# Optionally ask user whether to add a desktop file for "nvidia-settings"
+if ! [ -f "$HOME"/.local/share/applications/nvidia-settings.desktop ]; then
+  read -rp "Do you want to add a desktop file for \"nvidia-settings\"? (Y/n)" -n1 -s
+  echo
+  if [ "$REPLY" = Y ]; then
+    ln -sv /opt/nvidia/share/applications/nvidia-settings.desktop "$HOME"/.local/share/applications
+  fi
+fi
+echo -e "\e[32m\xf0\x9f\x91\x8f Installation completed!\e[m"
+
+# Optionally ask user whether to delete NVIDIA installer(s)
+read -rp "Do you want to delete NVIDIA driver installer(s) in current directory? (Y/n)" -n1 -s
+echo
+if [ "$REPLY" = Y ]; then
+  find . -maxdepth 1 -name 'NVIDIA-Linux-x86_64*\.run' -printf "Removing %f\n" -delete
+fi
+
 echo -e "\e[32m\xf0\x9f\x91\x8f Update completed!\e[m"
